4c3571801870b9b1df91d0d56357a863d71ac9a4
N.Mice <n.mice@mail.ru>
2021-05-07 02:30:11 +0300

    hw38-added wakeUp method

--- src/main/java/ru/neginskiy/subscounterbot/controller/WebHookController.java
+++ src/main/java/ru/neginskiy/subscounterbot/controller/WebHookController.java
@@ -1,5 +1,7 @@
 package ru.neginskiy.subscounterbot.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,13 @@ import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.neginskiy.subscounterbot.SubsCounterBot;
 
+import java.time.LocalDateTime;
+
 /**
  * Контроллер для обработки запроса от telegram-клиента
  */
 @RestController
+@Slf4j
 public class WebHookController {
     private final SubsCounterBot telegramBot;
 
@@ -21,5 +26,10 @@ public class WebHookController {
     @PostMapping("/")
     public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
         return telegramBot.onWebhookUpdateReceived(update);
+    }
+
+    @Scheduled(fixedDelay = 60000)
+    public void wakeUp() {
+        log.info("WAKE UP,LAZY BOT! localDateTime: {} ;-)", LocalDateTime.now());
     }
 }
